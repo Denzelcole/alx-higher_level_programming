f89ac2170c80d99e69b9dcbf2045c7b96ffb89b5
Denzelcole <theocole4@gmail.com>
2023-01-23 14:31:26 -0800

    restructure

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,12 @@
 
 
 def safe_print_list(my_list=[], x=0):
-	count = 0
-	for num in range(x):
-		try:
-			print("{}".format(my_list[num]), end="")
-			count = count + 1
-		except (ValueError, TypeError, IndexError):
-			break
-	print()
-	return count
+    i = 0
+    while i < x:
+        try:
+            print("{}".format(my_list[i]), end="")
+        except IndexError:
+            break
+        i += 1
+    print()
+    return i
