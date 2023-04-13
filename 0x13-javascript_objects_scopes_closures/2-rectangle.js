e80467d1d92f603b12e6d8a3d970d65ca873408a
Denzelcole <theocole4@gmail.com>
2023-04-12 21:17:33 -0700

    a zero rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor(w, h) {
-    //if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return {};
+  constructor (w, h) {
+    // If w or h is equal to 0 or not a positive integer, create empty object
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 };
