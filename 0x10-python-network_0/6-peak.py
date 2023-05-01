9a2ca3c9551c1c86eb6841385792e26f9c9da401
Denzelcole <theocole4@gmail.com>
2023-05-01 09:39:07 -0700

    refactoring the code

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""task 6 modele"""
+"""task 6 module"""
 
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
-    if list_of_integers == []:
+    """
+    Return a peak in a list of unsorted integers.
+    A peak is an element that is greater than or equal to its neighbors.
+    """
+    if not list_of_integers:
         return None
 
     size = len(list_of_integers)
@@ -13,7 +16,7 @@ def find_peak(list_of_integers):
     elif size == 2:
         return max(list_of_integers)
 
-    mid = int(size / 2)
+    mid = size // 2
     peak = list_of_integers[mid]
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
