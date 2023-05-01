76691f8a2c760fbc74737f974892f9ce0ac134c6
Denzelcole <theocole4@gmail.com>
2023-05-01 09:18:33 -0700

    Technical interview

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-
-"""
-    Interview preparation exercise
-    -- Algorithm must have lowest complexity
-"""
+"""task 6 modele"""
 
 
-def find_peak(list_int):
-    """ find the peak in a list of integers """
-    if list_int == []:
+def find_peak(list_of_integers):
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
         return None
-    elif len(list_int) == 1:
-        return list_int[0]
-    elif len(list_int) == 2:
-        return list_int[0] if list_int[0] > list_int[1] else list_int[1]
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
     else:
-        for i in range(1, (len(list_int) + 1) // 2):
-            back = len(list_int) - i - 1
-            if list_int[i] >= list_int[i - 1] and \
-               list_int[i] >= list_int[i + 1]:
-                return list_int[i]
-            elif list_int[back] >= list_int[back - 1] and list_int[back] >= \
-                    list_int[back + 1]:
-                return list_int[back]
-    return None
+        return find_peak(list_of_integers[mid + 1:])
