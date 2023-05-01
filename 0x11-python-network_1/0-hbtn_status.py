df59bcdd2e88a84ec3ce5b2ffdad46c85ad4fed9
Denzelcole <theocole4@gmail.com>
2023-05-01 10:25:45 -0700

    fetching  https://alx-intranet.hbtn.io/status using urlib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
+A python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-
-print('Body response:')
-print('\t- type: {}'.format(type(html)))
-print('\t- content: {}'.format(html))
-print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
